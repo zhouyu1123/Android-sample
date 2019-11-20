d7bdf74f2ab68f5d3b2a8969b647247d7fa0e2a1
zhouyu1123 <hubiguang_zy@163.com>
2019-11-20 16:46:55 +0800

    2.2.4 在活动中使用Toast

--- MyApplication3/app/src/main/java/com/example/myapplication/FirstActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/FirstActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -11,5 +14,19 @@ public class FirstActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1); //获取布局文件中定义的元素
+        button1.setOnClickListener(new View.OnClickListener() {   //按钮注册一个监听器
+            @Override
+            public void onClick(View v) {      // 点击按钮时，就会执行监听器onClick方法
+                /**
+                 *  函数: makeTest() 创建一个Toast对象
+                 *  参数1：Toast要求的上下文
+                 *  参数2： 显示文本的内容
+                 *  参数3：显示的时常
+                 */
+                Toast.makeText(FirstActivity.this,"you clicked Button 1",
+                Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
