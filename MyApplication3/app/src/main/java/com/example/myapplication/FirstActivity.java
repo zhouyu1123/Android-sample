16902d1c9369e8942670f063ce756b52c401a6e3
zhouyu1123 <hubiguang_zy@163.com>
2019-11-20 17:15:58 +0800

    2.2.5 在活动中使用Menu

--- MyApplication3/app/src/main/java/com/example/myapplication/FirstActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/FirstActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,7 +21,8 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {      // 点击按钮时，就会执行监听器onClick方法
                 /**
-                 *  函数: makeTest() 创建一个Toast对象
+                 *  函数: makeTest()
+                 *  功能：创建一个Toast对象
                  *  参数1：Toast要求的上下文
                  *  参数2： 显示文本的内容
                  *  参数3：显示的时常,有2两个内置参数选择，Toast.LENGTH_SHORT和Toast.LENGTH_LONG.
@@ -28,5 +31,38 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //重写方法 onCreateOptionsMenu
+        /**
+         *  函数： getMenuInflater()
+         *  功能：能够得到getMenuInflater对象
+         *  inflate接收两个参数
+         *  参数1:用于指定我们通过哪个资源文件夹来创建菜单
+         *  参数2：用于指定我们的菜单项将添加到哪一个Menu对象中
+         *  返回：Ture,表示允许创建的菜单显示出来，false无法显示
+         */
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    /**
+     * 函数： onOptionsItemSelected()
+     * 功能： 菜单响应事件
+     */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){ //通过调用getItemID来判断点击的是哪个菜单
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+                default:
+        }
+        return true;
     }
 }
